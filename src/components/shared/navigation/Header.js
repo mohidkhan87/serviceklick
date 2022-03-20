051bc8501c8d0dab6da5106c4a9f7a7990192f86
mohidkhan87 <mohidkhan87@gmail.com>
2022-03-20 02:45:24 -0700

    project setup

---
+++ src/components/shared/navigation/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+        <Link to='/newsroom'>
+            newsroom
+        </Link>
+    </div>
+  )
+}
+
+export default Header
