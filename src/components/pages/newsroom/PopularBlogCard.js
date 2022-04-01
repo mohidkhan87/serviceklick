5a5116c3646cab951b7efd041d86ee50bb8c2040
mohidkhan87 <mohidkhan87@gmail.com>
2022-04-01 19:06:40 +0500

    newsroom design fix

--- src/components/pages/newsroom/PopularBlogCard.js
+++ src/components/pages/newsroom/PopularBlogCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import Post from '../../../assets/images/pages/newsroom/post-2.png'
+import Post1 from '../../../assets/images/pages/newsroom/post-1.png'
 
 const BlogCard = ({item}) => {
   return (
     <div>
-      <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row gap-4">
         <div className="flex-1">
             <div className="relative">
-                <img src={Post} alt={item.title}/>
+                <img src={Post1} alt={item.title} className="w-full"/>
                 <div className="absolute bottom-4 left-4">
                   <div className="flex flex-col md:flex-row md:gap-4 md:items-center">
                     <button className="bg-white rounded-full text-secondary font-medium text-sm px-4 py-1">{item.category}</button>
@@ -19,7 +19,7 @@ const BlogCard = ({item}) => {
         </div>
         <div className="flex-1">
           <p className="text-darkGray font-medium text-xs">{item.date}</p>
-          <h2 className="text-primary font-bold text-lg md:text-2xl mt-2">{item.title}</h2>
+          <h2 className="text-primary font-bold text-lg md:text-base xl:text-xl mt-2">{item.title}</h2>
         </div>
       </div>
     </div>
--- src/pages/Newsroom.js
+++ src/pages/Newsroom.js
@@ -1,22 +1,44 @@
 import React from 'react'
 
-import Header from '../components/shared/navigation/Header'
+import MarginBottom from '../components/shared/spacing/MarginBottom'
 import Poster from '../components/pages/newsroom/Poster'
-import Blogs from '../components/pages/newsroom/Blogs'
+import Tabs from '../components/pages/newsroom/Tabs'
+import Posts from '../components/shared/Posts'
+import LoadButton from '../components/pages/newsroom/LoadButton'
 import PopularBlogs from '../components/pages/newsroom/PopularBlogs'
 import Banner from '../components/pages/newsroom/Banner'
-import FindAndHire from '../components/pages/newsroom/FindAndHire'
+import ServiceAd from '../components/shared/ServiceAd'
 import Footer from '../components/shared/navigation/Footer'
 
 const Newsroom = () => {
+  const postsData1 = [
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' },
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' },
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' },
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' }
+  ];
+  const postsData2 = [
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' },
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' },
+    { title: 'How to find local professionals for pretty much anything.',date:'February 20, 2022', category: 'News' }
+  ];
   return (
     <div>
-      <Header title="Serviceklick Newsroom" />
       <Poster />
-      <Blogs />
+      <MarginBottom margin={8} />
+      <Tabs />
+      <MarginBottom margin={8} />
+      <Posts postsData={postsData2} postsPerRow={3} />
+      <MarginBottom margin={16} />
+      <Posts postsData={postsData1} postsPerRow={4} />
+      <LoadButton />
+      <MarginBottom margin={16} />
       <PopularBlogs />
+      <MarginBottom margin={16} />
       <Banner />
-      <FindAndHire />
+      <MarginBottom margin={16} />
+      <ServiceAd />
+      <MarginBottom margin={16} />
       <Footer />
     </div>
   )
