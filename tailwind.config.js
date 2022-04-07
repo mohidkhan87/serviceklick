43047d63a49e3dde2803d3044f32fb653552a7ad
UsmanMurtaza92 <murtazausman041@gmail.com>
2022-04-07 11:41:07 -0700

    poster-curve

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,22 @@ module.exports = {
       colors: {
         primary: "#2B2D33",
         secondary: "#660019",
-        
+
         // Gray
         darkGray: "#818799",
         borderGray: "#EDEFF2",
         footerTextGray: "#81858E",
         lightGray: "#F5F7FA",
-        textGray: '#686868',
+        textGray: "#686868",
         customGray: "#C0C3CC",
 
+        lightBlue: "#A1D0E2",
         skyBlue: "#F5F7FA",
         lightPink: "#FCECE9",
         darkYellow: "#FCA101",
-        gradientPink: '#F9CFC6',
-        gradientBlue: '#B9D2EA',
-        textRed: '#E52322',
+        gradientPink: "#F9CFC6",
+        gradientBlue: "#B9D2EA",
+        textRed: "#E52322",
       },
       fontFamily: {
         sans: ["silka", ...defaultTheme.fontFamily.sans],
@@ -29,7 +30,7 @@ module.exports = {
       borderRadius: {
         ellipseCurve: "0 0 0 100%",
         ellipseCurveCenter: "0 0 50% 50%",
-        ellipseCurveCenterImage: "0 0 100px 10%"
+        ellipseCurveCenterImage: "0 0 100px 10%",
       },
       boxShadow: {
         fade: "10px 30px 150px 1px rgba(0, 0, 0, 0.20)",
@@ -37,12 +38,16 @@ module.exports = {
       backgroundImage: {
         "customer-bg":
           "url('/src/assets/images/pages/homepage/customer-bg.svg')",
-        'pinkBlueGradient': "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #F9CFC6 0%, #B9D2EA 100%)",
-        'magentaGradient': "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #660019 0%, #660019 100%)",
-        'greenGradient': "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #2F4121 0%, #2F4121 100%)",
-        'orangeGradient': "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #B33F0E 0%, #B33F0E 100%)",
-        'pinkGradient': "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #B11842 0%, #B11842 100%)",
-
+        pinkBlueGradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #F9CFC6 0%, #B9D2EA 100%)",
+        magentaGradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #660019 0%, #660019 100%)",
+        greenGradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #2F4121 0%, #2F4121 100%)",
+        orangeGradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #B33F0E 0%, #B33F0E 100%)",
+        pinkGradient:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.18), rgba(0, 0, 0, 0.18)), linear-gradient(167.96deg, #B11842 0%, #B11842 100%)",
       },
     },
   },
