8937809be9d2e406e94ba5cb2cffbcba2fd81a5e
mohidkhan87 <mohidkhan87@gmail.com>
2022-04-07 11:57:33 -0700

    New Section Update

--- src/components/pages/homepage/ReviewPlatform.js
+++ src/components/pages/homepage/ReviewPlatform.js
@@ -1,11 +1,29 @@
 import React from 'react'
+
 import Container from '../../ui/Container'
+
+import Trustpilot from '../../../assets/images/pages/homepage/trustpilot.png'
+import GoogleReviews from '../../../assets/images/pages/homepage/google-reviews.png'
 
 const ReviewPlatform = () => {
   return (
     <div>
         <Container>
-            asd
+            <div className='flex flex-col gap-5 md:flex-row mx-auto w-max'>
+              <div className='flex items-center'>
+                <p className='text-darkGray 2xl:text-lg'>What customers say about our services on </p>
+              </div>
+              <div>
+                <div className='flex gap-5'>
+                  <div className='w-max'>
+                    <img src={GoogleReviews} alt="Google Reviews" className='w-full' />
+                  </div>
+                  <div className='w-max'>
+                    <img src={Trustpilot} alt="Trustpilot" className='w-full' />
+                  </div>
+                </div>
+              </div>
+            </div>
         </Container>
     </div>
   )
--- src/components/shared/navigation/Footer.js
+++ src/components/shared/navigation/Footer.js
@@ -94,7 +94,7 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div className="flex-1 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-x-5 gap-y-8">
+          <div className="flex-1 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-x-5 gap-y-0">
             <div className="col-span-1">
               <h4 className="text-primary text-lg 2xl:text-xl font-bold">Customer's</h4>
               <div className="mt-4">
